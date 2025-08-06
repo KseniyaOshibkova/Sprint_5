d0d4cedfd5912a755de1012cc19e5082ec1815be
KseniyaOshibkova <roksta95@gmail.com>
2025-08-06 19:32:46 +0500

    refactor

--- pages/create_lisiting_page/create_lisiting_page.py
+++ pages/create_lisiting_page/create_lisiting_page.py
@@ -1,8 +1,5 @@
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-
-from data.url import Url
 from locators.locators import Locators
 from pages.base_page.base_page import BasePage
 
